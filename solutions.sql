af2f118aa48158a4fec23caff9d262c5665ac7a4
Jason Ouyang <jasonouyang112@gmail.com>
2021-03-30 21:07:24 -0700

    solution and erb

---
+++ solutions.sql
@@ -0,0 +1,52 @@
+1.
+SELECT title FROM books
+JOIN authors ON books.authorId = authors.id
+WHERE authors.name like 'Lutz%Mark'
+2.
+SELECT title FROM books
+JOIN subjects ON books.subjectId = subjects.id
+WHERE subjects.name = 'Childrens Books';
+
+3. 
+SELECT DISTINCT name FROM subjects
+JOIN books ON books.subjectId = subjects.id;
+
+4. (More informative)
+SELECT DISTINCT authors.name
+FROM authors
+JOIN books ON authors.id = books.authorId
+JOIN subjects ON subjects.id = books.subjectId;
+
+5.
+SELECT books.title 
+FROM books
+JOIN authors ON books.authorId = authors.id 
+WHERE authors.name = 'Hogarth Burne';
+
+6.
+SELECT authors.name
+FROM authors
+JOIN books ON authors.id = books.authorId
+WHERE books.title = 'Little Women';
+
+7. INSERT INTO authors(name)
+   VALUES('Jason Ouyang');
+8. 
+ INSERT INTO books(title,authorId,subjectId)
+   VALUES('Bob the Builder',10,5);
+9.
+UPDATE authors 
+SET name = 'Stephen King'
+WHERE id = 10;
+
+10. 
+SELECT subjects.name 
+FROM subjects
+JOIN books ON  subjects.id = books.subjectId
+WHERE books.title = '2001: A Space Odyssey'; 
+
+11.
+SELECT books.title
+FROM books
+JOIN subjects ON subjects.id = books.subjectId
+WHERE subjects.name  like '%Science%';
